217b80f4c40eaa67115243fae82309e74ae1bf9d
Alex <alex@hagl.de>
2024-07-14 23:43:13 +0200

    Includes abstraction for the db context

---
+++ Abstractions/ILogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace N35T.Distributed;
+
+public interface ILogDbContext {
+
+    public DbSet<DistributedActionLog> DistributedActionLog {get;}
+
+    public Task<int> SaveChangesAsync();
+
+    public Task ClearTable(string tableName);
+}
