cd7cd82c62436740c331858ac85da909f4e0b2db
Alex <alex@hagl.de>
2024-07-14 23:43:33 +0200

    Adds logrepository interface

---
+++ Repositories/Interfaces/ILogRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace N35T.Distributed;
+
+public interface ILogRepository {
+
+    Task LogUpdate(string tableName, string refId, string column, string newVal);
+    Task LogInsert(string tableName, string refId, string column, string insertedVal);
+    Task LogDelete(string tableName, string refId);
+    Task LogSync();
+    Task ClearLocalLogs();
+
+    Task<List<DistributedActionLog>> GetAllLogs();
+    Task<List<DistributedActionLog>> GetAllLogsSince(DateTimeOffset timestamp);
+}
