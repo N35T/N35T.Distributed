3014b2223677e8b5aca9cecc8831bdfabd418a18
Alex <alex@hagl.de>
2024-08-01 21:28:54 +0200

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,9 @@
 read -p "Enter package version: " version
+read -p "Enter the version message: " message
 read -p "Enter NuGet API Key: " apiKey
+
+git tag -a $version -m "$message"
+git push origin $version
 
 dotnet pack
 dotnet nuget push bin/Release/N35T.Distributed.$version.nupkg --api-key $apiKey --source https://api.nuget.org/v3/index.json
